9a40560c6e0c5b4226d74b2c28c0bd76f0ba6a15
Dmytro Kolosovskyi <kolosovskyi.dmytro@gmail.com>
2023-08-14 22:32:37 +0200

    Full project commit

---
+++ src/Drum.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { AudioClip } from "./types";
+
+type DrumProps = {
+  audioClip: AudioClip;
+};
+
+const Drum = ({ audioClip }: DrumProps) => {
+  const playSound = (clip: AudioClip) => {
+    (document.getElementById(clip.keyTrigger) as HTMLAudioElement)
+      .play()
+      .catch(console.error);
+    document.getElementById("display")!.innerText = clip.description;
+  };
+
+  return (
+    <div>
+      <button
+        className="drum-pad"
+        id={`drum-${audioClip.keyTrigger}`}
+        onClick={() => playSound(audioClip)}
+      >
+        <audio src={audioClip.url} id={audioClip.keyTrigger} className="clip" />
+        {audioClip.keyTrigger}
+      </button>
+    </div>
+  );
+};
+
+export default Drum;
